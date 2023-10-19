b6963bf31230b3880c88310b7c5c8cd56141200e
JorgeGWD <jorge.rodriguez.r@gmail.com>
2023-10-19 17:56:10 -0500

    Consume API

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
-import React from 'react';
+import { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+
+    const [characters, setCharacters] = useState([])
+
+    const fetchData = () => {
+        fetch('https://rickandmortyapi.com/api/character')
+        .then(response => response.json())
+        .then(data => setCharacters(data.results))
+        .catch(err => err)
+    }
+
+    useEffect(() => {
+        fetchData()
+    }, [])
+
     return (
         <div className="App">
             <h1>PWS Template Install</h1>
+            {
+                characters.map((character) => (
+                    <section key={character.id}>
+                        <p>{character.name}</p>
+                    </section>
+                ))
+            }
         </div>
     );
 }
