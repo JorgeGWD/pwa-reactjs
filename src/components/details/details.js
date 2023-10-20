ce8d576cb4766196eb55293026bbf02fde2b829c
JorgeGWD <jorge.rodriguez.r@gmail.com>
2023-10-20 18:12:29 -0500

    Fix component to load data

--- src/components/details/details.js
+++ src/components/details/details.js
@@ -1,30 +1,40 @@
-import { useState, useEffect } from 'react'
-import { fetchData } from '../../services/fetchData'
-import { useParams } from 'react-router'
-import './details.css'
+import React, { useState, useEffect } from 'react';
+import { fetchData } from '../../services/fetchData';
+import { useParams } from 'react-router';
+import './details.css';
 
 export const Details = () => {
+    const [character, setCharacter] = useState([]);
+    const requestData = 'character';
+    const { id } = useParams();
 
-    const [characters, setCharacters] = useState({})
-    const requestData = 'character'
-    const { id } = useParams()
+    const getCharacter = async (id) => {
+        const response = await fetchData({
+            requestData,
+            id,
+        });
+        setCharacter(response)
+    }
 
     useEffect(() => {
-        fetchData({setCharacters, requestData, id})
-    }, [id])
-    
+        getCharacter(id)
+    }, [id]);
+
     return (
         <main className='details'>
-            <h1>Character Details</h1>
-            <section className='detail' key={characters.id}>
-                <img src={characters.image} alt={characters.name} />
-                <article>
-                    <h2>{characters.name}</h2>
-                    <h3>Species: {characters.species}</h3>
-                    <h3>Gender: {characters.gender}</h3>
-                </article>
-            </section>
+        <h1>Character Details</h1>
+        {
+            character.map((character) => (
+                <section className='detail' key={character.id}>
+                    <img src={character.image} alt={character.name} />
+                    <article>
+                        <h2>{character.name}</h2>
+                        <h3>Species: {character.species}</h3>
+                        <h3>Gender: {character.gender}</h3>
+                    </article>
+                </section>
+            ))
+        }
         </main>
     );
-    
-}
+};
