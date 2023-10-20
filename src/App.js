359552401b9b929efff8d8f1cfa2ffc909ac5510
JorgeGWD <jorge.rodriguez.r@gmail.com>
2023-10-20 10:47:30 -0500

    Clean App file and start to import components

--- src/App.js
+++ src/App.js
@@ -1,31 +1,10 @@
-import { useState, useEffect } from 'react';
 import './App.css';
+import { Card } from './components/card/card';
 
 function App() {
-
-    const [characters, setCharacters] = useState([])
-
-    const fetchData = () => {
-        fetch('https://rickandmortyapi.com/api/character')
-        .then(response => response.json())
-        .then(data => setCharacters(data.results))
-        .catch(err => err)
-    }
-
-    useEffect(() => {
-        fetchData()
-    }, [])
-
     return (
         <div className="App">
-            <h1>PWS Template Install</h1>
-            {
-                characters.map((character) => (
-                    <section key={character.id}>
-                        <p>{character.name}</p>
-                    </section>
-                ))
-            }
+           <Card />
         </div>
     );
 }
