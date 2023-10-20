165081b59a89b776b08852e119736b449d13b890
JorgeGWD <jorge.rodriguez.r@gmail.com>
2023-10-20 16:12:09 -0500

    Add pagination button and logic in both files

--- src/components/card/card.js
+++ src/components/card/card.js
@@ -1,28 +1,38 @@
 import { useState, useEffect } from 'react';
 import { fetchData } from '../../services/fetchData'
-import './card.css'
 import { Link } from 'react-router-dom';
+import './card.css'
 
 export const Card = () => {
 
     const [characters, setCharacters] = useState([])
+    const [pageNumber, setPageNumber] = useState(1)
     const requestData = 'character'
 
+    const loadMoreCharacters = () => {
+        setPageNumber(prevPageNumber => prevPageNumber + 1)
+    }
+
     useEffect(() => {
-        fetchData({setCharacters, requestData})
-    }, [])
+        fetchData({setCharacters, requestData, page: pageNumber})
+    }, [pageNumber])
+
 
     return (
-        <section className='card'>
-            {
-                characters.map((character) => (
-                    <Link to={`/character-details/${character.id}`} key={character.id}>
-                        <article >
-                            <img src={character.image} alt={character.name} />
-                        </article>
-                    </Link>
-                ))
-            }
-        </section>
+        <main className='cards'>
+            <h1>Characters</h1>
+            <section className='card'>
+                {
+                    characters.map((character) => (
+                        <Link to={`/character-details/${character.id}`} key={character.id}>
+                            <article >
+                                <img src={character.image} alt={character.name} />
+                            </article>
+                        </Link>
+                    ))
+                }
+            </section>
+            <button onClick={loadMoreCharacters}>Load More</button>
+        </main>
     )
 }
--- src/services/fetchData.js
+++ src/services/fetchData.js
@@ -1,15 +1,19 @@
-export const fetchData = ({ setCharacters, requestData, id = null }) => {
-    const API_URL = id
-      ? `https://rickandmortyapi.com/api/${requestData}/${id}`
-      : `https://rickandmortyapi.com/api/${requestData}`;
-  
-    fetch(API_URL)
-      .then(response => response.json())
-      .then(data => {
-        // Verificar si data tiene una propiedad results
-        const charactersData = data.results ? data.results : data;
-  
-        setCharacters(charactersData);
-      })
-      .catch(err => console.error(err));
-  };
+export const fetchData = ({ setCharacters, requestData, id = null, page = 1 }) => {
+        const API_URL = id
+        ? `https://rickandmortyapi.com/api/${requestData}/${id}`
+        : `https://rickandmortyapi.com/api/${requestData}?page=${page}`;
+    
+        fetch(API_URL)
+        .then(response => response.json())
+        .then(data => {
+            // Check if data has a results property
+            const charactersData = data.results ? data.results : data;
+    
+            setCharacters((prevCharacters) => {
+                // Make sure prevCharacters is an array
+                const existingCharacters = Array.isArray(prevCharacters) ? prevCharacters : [];
+                return [...existingCharacters, ...charactersData];
+            });
+        })
+        .catch(err => console.error(err));
+};
