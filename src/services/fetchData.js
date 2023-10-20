8720d1c08cab0d3b55f16885145a7d32d4e14ca7
JorgeGWD <jorge.rodriguez.r@gmail.com>
2023-10-20 18:09:07 -0500

    Ajust fetchData to load only necessary data

--- src/services/fetchData.js
+++ src/services/fetchData.js
@@ -1,19 +1,15 @@
-export const fetchData = ({ setCharacters, requestData, id = null, page = 1 }) => {
-        const API_URL = id
+export const fetchData = ({ requestData, id = null, page = 1 }) => {
+    const API_URL = id
         ? `https://rickandmortyapi.com/api/${requestData}/${id}`
         : `https://rickandmortyapi.com/api/${requestData}?page=${page}`;
-    
-        fetch(API_URL)
+
+    return fetch(API_URL)
         .then(response => response.json())
         .then(data => {
             // Check if data has a results property
-            const charactersData = data.results ? data.results : data;
-    
-            setCharacters((prevCharacters) => {
-                // Make sure prevCharacters is an array
-                const existingCharacters = Array.isArray(prevCharacters) ? prevCharacters : [];
-                return [...existingCharacters, ...charactersData];
-            });
+            const charactersData = data.results ? data.results : [data];
+            
+            return charactersData;
         })
         .catch(err => console.error(err));
 };
