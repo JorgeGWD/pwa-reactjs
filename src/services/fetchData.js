a54c432ad28b862513b24142ebc8917775cf4999
JorgeGWD <jorge.rodriguez.r@gmail.com>
2023-10-20 12:22:02 -0500

    Adjust fetchData file to receive props in API_URL and toggle data

--- src/services/fetchData.js
+++ src/services/fetchData.js
@@ -1,8 +1,15 @@
-const API_URL = `https://rickandmortyapi.com/api/`
-
-export const fetchData = (setCharacters) => {
-    fetch(`${API_URL}character`)
-    .then(response => response.json())
-    .then(data => setCharacters(data.results))
-    .catch(err => err)
-}
+export const fetchData = ({ setCharacters, requestData, id = null }) => {
+    const API_URL = id
+      ? `https://rickandmortyapi.com/api/${requestData}/${id}`
+      : `https://rickandmortyapi.com/api/${requestData}`;
+  
+    fetch(API_URL)
+      .then(response => response.json())
+      .then(data => {
+        // Verificar si data tiene una propiedad results
+        const charactersData = data.results ? data.results : data;
+  
+        setCharacters(charactersData);
+      })
+      .catch(err => console.error(err));
+  };
