0a558c51dddeacb271087a17f6dc11219233abd7
JorgeGWD <jorge.rodriguez.r@gmail.com>
2023-10-21 12:00:02 -0500

    Import generalInfo component and ajust responsive style

--- src/components/details/details.js
+++ src/components/details/details.js
@@ -1,19 +1,20 @@
-import React, { useState, useEffect } from 'react';
-import { fetchData } from '../../services/fetchData';
-import { useParams } from 'react-router';
-import './details.css';
-import { Button } from '../common/button/button';
+import { useState, useEffect } from 'react'
+import { fetchData } from '../../services/fetchData'
+import { useParams } from 'react-router'
+import './details.css'
+import { Button } from '../common/button/button'
+import GeneralInfo from '../generalInfo/generalInfo'
 
 export const Details = () => {
-    const [character, setCharacter] = useState([]);
-    const requestData = 'character';
-    const { id } = useParams();
+    const [character, setCharacter] = useState([])
+    const requestData = 'character'
+    const { id } = useParams()
 
     const getCharacter = async (id) => {
         const response = await fetchData({
             requestData,
             id,
-        });
+        })
         setCharacter(response)
     }
 
@@ -23,26 +24,29 @@ export const Details = () => {
 
     useEffect(() => {
         getCharacter(id)
-    }, [id]);
+    }, [id])
 
     return (
         <main className='details'>
-        <h1>Character Details</h1>
-        <section className='detail'>
-            {
-                character.map((character) => (
-                    <article className='detail__card' key={character.id}>
-                        <img src={character.image} alt={character.name} />
-                        <div className='detail__card-info'>
-                            <h2>{character.name}</h2>
-                            <h3>Species: {character.species}</h3>
-                            <h3>Gender: {character.gender}</h3>
-                        </div>
-                    </article>
-                ))
-            }
-        </section>
-        <Button className='btn-principal' text='Back' onClick={returnPage}/>
+            <h1>Character Details</h1>
+            <div className="details__content">
+                <section className='detail'>
+                    {
+                        character.map((character) => (
+                            <article className='detail__card' key={character.id}>
+                                <img src={character.image} alt={character.name} />
+                                <div className='detail__card-info'>
+                                    <h2>{character.name}</h2>
+                                    <h3>Species: {character.species}</h3>
+                                    <h3>Gender: {character.gender}</h3>
+                                </div>
+                            </article>
+                        ))
+                    }
+                </section>
+                <GeneralInfo />
+            </div>
+            <Button className='btn-principal' text='Back' onClick={returnPage}/>
         </main>
-    );
-};
+    )
+}
