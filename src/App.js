99d99a0886401bba372243413c4988d748b7afd8
JorgeGWD <jorge.rodriguez.r@gmail.com>
2023-10-21 14:58:12 -0500

    Create and configurate 404 page

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import './App.css'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import { Card } from './components/card/card'
 import { Details } from './components/details/details'
+import { Error404 } from './components/common/404/404'
 
 const router = createBrowserRouter([
     {
         path: '/',
         element: <Card />,
-        errorElement: <h1>Error</h1>
+        errorElement: <Error404 />
     },
     {
         path: '/character-details/:id',
---
+++ src/components/common/404/404.js
@@ -0,0 +1,9 @@
+import './404.css'
+
+export const Error404 = () => {
+  return (
+    <section className='error-404'>
+        <h1>404 Page not faund.</h1>
+    </section>
+  )
+}
